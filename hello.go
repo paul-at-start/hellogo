ba0370dd59e381a8e7371bd5865e26b026c8f7bb
Paul <paul@dagbladet.no>
2015-06-05 13:13:25 +0200

    Wip: Working http handler with output

--- hello.go
+++ hello.go
@@ -4,8 +4,39 @@
 
 package main
 
-import "fmt"
+import (
+	"image/jpeg"
+	"net/http"
+	"os"
+)
+import "github.com/disintegration/imaging"
 
 func main() {
-    fmt.Printf("hello, world\n")
+	http.HandleFunc("/makeImg", blurImage)
+	http.ListenAndServe(":6060", nil)
+}
+
+func blurImage(rw http.ResponseWriter, r *http.Request) {
+
+	osr, err := os.OpenFile("file.jpg", os.O_RDONLY, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	srcImage, err := jpeg.Decode(osr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	dstImage := imaging.Blur(srcImage, 13.5)
+	//err = imaging.Save(dstImage, "file2.jpg")
+
+	err = jpeg.Encode(rw, dstImage, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
 }

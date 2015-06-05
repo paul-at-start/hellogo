9416568103efdedcf196f9e42558c6692b63d27f
Paul <paul@dagbladet.no>
2015-06-05 13:29:44 +0200

    Wip: Http param input

--- hello.go
+++ hello.go
@@ -7,30 +7,34 @@ package main
 import (
 	"image/jpeg"
 	"net/http"
-	"os"
+	"runtime"
 )
 import "github.com/disintegration/imaging"
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	http.HandleFunc("/makeImg", blurImage)
 	http.ListenAndServe(":6060", nil)
 }
 
 func blurImage(rw http.ResponseWriter, r *http.Request) {
 
-	osr, err := os.OpenFile("file.jpg", os.O_RDONLY, 0666)
+	//osr, err := os.OpenFile("file.jpg", os.O_RDONLY, 0666)
+
+	resp, err := http.Get(r.URL.Query().Get("img"))
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	srcImage, err := jpeg.Decode(resp.Body)
 
 	if err != nil {
 		panic(err)
 	}
 
-	srcImage, err := jpeg.Decode(osr)
-
-	if err != nil {
-		panic(err)
-	}
-
-	dstImage := imaging.Blur(srcImage, 13.5)
+	dstImage := imaging.Blur(srcImage, 28.5)
 	//err = imaging.Save(dstImage, "file2.jpg")
 
 	err = jpeg.Encode(rw, dstImage, nil)
